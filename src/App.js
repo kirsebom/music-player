f2568fb07efd6d962278bfc5dd9eea2cd02e6e57
kirsebom <59082697+kirsebom@users.noreply.github.com>
2022-07-27 14:48:30 +0200

    music player

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import { useState, useRef } from "react";
+import "./style/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./data";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+
+function App() {
+	const audioRef = useRef(null);
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [songInfo, setSongInfo] = useState({
+		currentTime: 0,
+		duration: 0,
+		animationPercentage: 0,
+	});
+	const [libraryStatus, setLibraryStatus] = useState(false);
+
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime;
+		const duration = e.target.duration;
+
+		const roundedCurrent = Math.round(current);
+		const roundedDuration = Math.round(duration);
+		const animation = Math.round((roundedCurrent * 100) / roundedDuration);
+
+		setSongInfo({
+			...songInfo,
+			currentTime: current,
+			duration,
+			animationPercentage: animation,
+		});
+	};
+
+	const songEndHandler = async () => {
+		let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+		await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+		if (isPlaying) {
+			setTimeout(() => {
+				audioRef.current.play();
+			}, 100);
+		}
+	};
+
+	return (
+		<div className={`App ${libraryStatus ? "library-active" : ""}`}>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+			<Song currentSong={currentSong} />
+			<Player
+				audioRef={audioRef}
+				currentSong={currentSong}
+				isPlaying={isPlaying}
+				setIsPlaying={setIsPlaying}
+				setSongInfo={setSongInfo}
+				songInfo={songInfo}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				setSongs={setSongs}
+			/>
+			<Library
+				audioRef={audioRef}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				isPlaying={isPlaying}
+				setSongs={setSongs}
+				libraryStatus={libraryStatus}
+			/>
+			<audio
+				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+				onEnded={songEndHandler}
+			></audio>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,248 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+	return [
+		{
+			name: "Assembly",
+			artist: "Dotlights, Almøst Silent",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+			id: uuidv4(),
+			active: true,
+			color: ["#3327B8", "#4A2573"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=35663",
+		},
+		{
+			name: "Rooftop Breeze",
+			artist: "Smile High, Richard Vagner",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/05/42878bcb02c1a3613f4913b226674cd36bac9830-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#2A4476", "#C69701"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=36650",
+		},
+		{
+			name: "Gravity",
+			artist: "Knowmadic",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#DC7271", "#DE9294"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=41648",
+		},
+		{
+			name: "Childhood Dream",
+			artist: "Masked Man",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#3327B8", "#4A2573"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=35672",
+		},
+		{
+			name: "After Hours",
+			artist: "Poldoore",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/05/36fcc61f2d1c707e99f36706700d73229e2e8dff-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#E1D6BA", "#E1D5B9"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=36942",
+		},
+		{
+			name: "Humility Piece",
+			artist: "Psalm Trees",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/05/ce636ede9d5fc9fc2417ce40af98060fbcbdadca-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#597B48", "#064379"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=36905",
+		},
+		{
+			name: "Highs and Lows",
+			artist: "Kissamilé",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/05/9bb8c31852704fe0177b21ee1f674b71027b268f-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#96A99E", "#587B44"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=36944",
+		},
+		{
+			name: "Sundials",
+			artist: "chief., Shrimpnose",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/07/4f05da370f675eb78a6b92880f93c4e7787935e3-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#C7A4BA", "#ECC5BF"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=41687",
+		},
+		{
+			name: "Changing Winds",
+			artist: "Knowmadic",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#DC7271", "#DE9294"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=41654",
+		},
+		{
+			name: "Passing Time",
+			artist: "Knowmadic",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#DC7271", "#DE9294"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=41652",
+		},
+		{
+			name: "Street Lights",
+			artist: "Knowmadic",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#DC7271", "#DE9294"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=41650",
+		},
+		{
+			name: "Chill'n",
+			artist: "C Y G N",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/04/8cc8bdc59b30dd1f9d401cb485b8537d909e1f50-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#3E8C2E", "#E9DEDA"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=31610",
+		},
+		{
+			name: "Chilled Memories",
+			artist: "C Y G N",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/04/8cc8bdc59b30dd1f9d401cb485b8537d909e1f50-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#3E8C2E", "#E9DEDA"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=31615",
+		},
+		{
+			name: "Rain Dance",
+			artist: "Screen Jazzmaster, Viktor Minsky",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/01/5753da482a6839b31e4905b22a2f8d65913e7eb4-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#781335", "#5389A8"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=30094",
+		},
+		{
+			name: "Two Thousand Miles",
+			artist: "Aviino",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2021/09/a2d710149876c8e0c9fb097840b2438fbc21bd37-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#AE5D39", "#56707F"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=23314",
+		},
+		{
+			name: "Sketchbook Memory",
+			artist: "Aviino",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2021/09/a2d710149876c8e0c9fb097840b2438fbc21bd37-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#AE5D39", "#56707F"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=23061",
+		},
+		{
+			name: "I Main Samus Now",
+			artist: "Sleepy Fish",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2021/09/2ce75252f5419a45d76bb93424ac1eae3e688b17-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#F9C39C", "#82CFE0"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=24822",
+		},
+		{
+			name: "Falling Short",
+			artist: "Hanz",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2021/08/b0bb2309d0c8fe0a32907ecddab689501b7de5cf-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#937484", "#526584"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=24643",
+		},
+		{
+			name: "Looking In",
+			artist: "Smile High, Teddy Roxpin",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#458C94", "#A6D8D5"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=23335",
+		},
+		{
+			name: "Where The Sun Goes",
+			artist: "Delayde, anybodyy",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2021/09/6a9ef8ac00e168d3308fdf9e336874c03fab829d-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#8860D1", "#6BBFA4"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=23188",
+		},
+		{
+			name: "Stay Positive",
+			artist: "Aves",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2022/04/73f23095038f6f023083520706f3e5345f5dd682-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#EF97BE", "#F8F6BB"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=32859",
+		},
+		{
+			name: "Yesterday",
+			artist: "chief.",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/6e722fd3ec0283a30b55e16b5c7228f311555133-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#B5B694", "#DE6B27"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=8503",
+		},
+		{
+			name: "Coo Lullaby",
+			artist: "Masked Man",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/774a0958326fec3fd21e32a500287729179d12de-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#F04663", "#F6B6BC"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=8156",
+		},
+		{
+			name: "Belly Breathing",
+			artist: "Birocratic",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/5c2d5b05dfc98afb5ed850ca918f732445b8ca1e-1024x1024.jpg",
+			id: uuidv4(),
+			active: false,
+			color: ["#9F6F82", "#81C9B4"],
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=8675",
+		},
+	];
+}
+
+export default chillHop;
